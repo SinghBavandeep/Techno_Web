9dd5ec453d84e1f76197f98ffec3a3bbc58d5a47
SinghBavandeep <115554428+SinghBavandeep@users.noreply.github.com>
2024-03-28 19:52:36 +0100

    first commit

--- front-skeleton/src/app/models/links.model.ts
+++ front-skeleton/src/app/models/links.model.ts
@@ -1,4 +1,7 @@
 export interface Link {
   name: string
   href: string
+  underlineStyle?: { [key: string]: string | number }; // Ajout de la propriété
+  // Vous pouvez aussi ajouter iconStyle si nécessaire
+  iconStyle?: { [key: string]: string | number };
 }
--- front-skeleton/src/app/navbar/navbar.component.ts
+++ front-skeleton/src/app/navbar/navbar.component.ts
@@ -1,16 +1,34 @@
-import { Component } from "@angular/core"
-import { Link } from "models/links.model"
+import { Component } from "@angular/core";
+import { Link } from "models/links.model";
 
 @Component({
-  selector: "navbar",
+  selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent {
-  links: Link[] = []
+  links: Link[] = [
+    { name: "Accueil", href: "/etudiants" },
+    { name: "Services", href: "/services" },
+    { name: "À propos", href: "/about" },
+    { name: "Contact", href: "/contact" }
+  ];
 
-  constructor() {
-    this.links.push({ name: "Étudiants", href: "etudiants" })
-    this.links.push({ name: "Filières", href: "filieres" })
+  constructor() {}
+
+  toggleUnderline(link: Link, isHovering: boolean): void {
+    if (isHovering) {
+      link.underlineStyle = { width: '70%' };
+      link.iconStyle = {
+        opacity: '1',
+        transform: 'translateX(-40%)' // Image will now start from its original position
+      };
+    } else {
+      link.underlineStyle = { width: '0' };
+      link.iconStyle = {
+        opacity: '0',
+        transform: 'translateX(-150%)' // Return to centered when not hovering
+      };
+    }
   }
 }

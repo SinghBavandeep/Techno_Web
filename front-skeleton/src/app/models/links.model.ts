3957e9dbbb3fdb95ee8b7597f6d7659f8e110c23
SinghBavandeep <115554428+SinghBavandeep@users.noreply.github.com>
2024-04-20 16:57:58 +0200

    Delete front-skeleton directory

--- front-skeleton/src/app/models/links.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Link {
-  name: string
-  href: string
-  underlineStyle?: { [key: string]: string | number }; // Ajout de la propriété
-  // Vous pouvez aussi ajouter iconStyle si nécessaire
-  iconStyle?: { [key: string]: string | number };
-}
